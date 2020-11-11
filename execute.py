b8de534d3f952d0993ee5396e13697f22b24c6dc
krocky-cooky <64895404+krocky-cooky@users.noreply.github.com>
2020-11-11 23:26:08 +0900

    process

--- execute.py
+++ execute.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 
@@ -12,20 +14,7 @@ t = digit.target
 t = np.identity(10)[t]
 x_train,x_test,t_train,t_test = train_test_split(x,t,random_state=0)
 
-if __name__ == '__main__':
-    network = neuralNetwork(epoch=3000,learning_rate=0.0002)
-    layer_list = [64,[100,70],10]
-    network.set_layer(layer_list)
-    (elapsed_time,train_acc) = network.train(x_train,t_train)
-    y_test = network.predict(x_test)
-    loss = network.loss(y_test,t_test)
-    accuracy = network.accuracy(x_test,t_test)
-    print('\n\n===========test case results=========')
-    print('loss : {}'.format(loss))
-    print('accuracy : {}\n'.format(accuracy))
-
-
-
+def save_result():
     path = './results.txt'
     with open(path) as f:
         text = f.read()
@@ -44,4 +33,34 @@ if __name__ == '__main__':
 
     with open(path,mode='w') as f:
         f.write(text)
+
+def generate_heatmap(n):
+    for i in range(1,len(n.layers)):
+        if i == 1:
+            w = n.layers[1].weight
+        else:
+            w = np.dot(w,n.layers[i].weight)
+    w = w.reshape(10,8,8)
+    for i in range(10):
+        plt.figure()
+        sns.heatmap(w[i])
+        plt.show()
+
+if __name__ == '__main__':
+    network = neuralNetwork(epoch=30000,learning_rate=0.0002)
+    layer_list = [64,[100,70],10]
+    network.set_layer(layer_list)
+    (elapsed_time,train_acc) = network.train(x_train,t_train)
+    y_test = network.predict(x_test)
+    loss = network.loss(y_test,t_test)
+    accuracy = network.accuracy(x_test,t_test)
+    print('\n\n===========test case results=========')
+    print('loss : {}'.format(loss))
+    print('accuracy : {}\n'.format(accuracy))
+
+    #save_result()
+    generate_heatmap(network)
+
+
+    
 

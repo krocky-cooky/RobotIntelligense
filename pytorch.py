fd390ec86a34277b945a7e290d8c91eeebe85c1a
krocky-cooky <64895404+krocky-cooky@users.noreply.github.com>
2020-12-15 15:08:50 +0900

    update pytorch.py

--- pytorch.py
+++ pytorch.py
@@ -1,106 +1,135 @@
-import numpy as np
+import torch
+import torch.nn.functional as f
+from torch.utils.data import DataLoader
+from torchvision import datasets,transforms
 import matplotlib.pyplot as plt
-import torch 
 import torch.nn as nn
+import numpy as np
 import torch.optim as optimizers
-from sklearn.metrics import accuracy_score
 
-x_train = np.load('./datasets/kmnist-train-imgs.npz')['arr_0']
-t_train = np.load('./datasets/kmnist-train-labels.npz')['arr_0']
-x_test = np.load('./datasets/kmnist-test-imgs.npz')['arr_0']
-t_test = np.load('./datasets/kmnist-test-labels.npz')['arr_0']
-
-t_train = np.identity(10)[t_train]
-t_test = np.identity(10)[t_test]
-x_train = x_train.reshape((60000,-1))
-x_test = x_test.reshape((10000,-1))
-x_train = x_train/255
-x_test = x_test/255
-
-class MLP(nn.Module):
+class Net(nn.Module):
     def __init__(
         self,
         input_dim,
-        hidden_dim,
-        output_dim,
+        hidden_dim_1,
+        hidden_dim_2,
+        output_dim
     ):
         super().__init__()
-        self.l1 = nn.Linear(input_dim,hidden_dim)
+        self.l1 = nn.Linear(input_dim,hidden_dim_1)
         self.a1 = nn.ReLU()
-        self.l2 = nn.Linear(hidden_dim,output_dim)
-        self.a2 = nn.Sigmoid()
+        self.l2 = nn.Linear(hidden_dim_1,hidden_dim_2)
+        self.a2 = nn.ReLU()
+        self.l3 = nn.Linear(hidden_dim_2,output_dim)
+        self.a3 = nn.Softmax(dim = 1)
         
-        
-        self.layers = [self.l1,self.a1,self.l2,self.a2]
+        self.layers = [self.l1,self.a1,self.l2,self.a2,self.l3,self.a3]
         
     def forward(self,x):
         for layer in self.layers:
             x = layer(x)
+            
         return x
 
+class Dataset:
+    def __init__(self,train = True):
+        x_train = np.load('./datasets/kmnist-train-imgs.npz')['arr_0']
+        t_train = np.load('./datasets/kmnist-train-labels.npz')['arr_0']
+        x_test = np.load('./datasets/kmnist-test-imgs.npz')['arr_0']
+        t_test = np.load('./datasets/kmnist-test-labels.npz')['arr_0']
+
+        t_train = np.identity(10)[t_train]
+        t_test = np.identity(10)[t_test]
+        x_train = x_train.reshape((60000,-1)).astype(float)
+        x_test = x_test.reshape((10000,-1)).astype(float)
+        x_train = x_train/255
+        x_test = x_test/255
+        if train:
+            #self.x = torch.from_numpy(x_train).float
+            #self.t = torch.from_numpy(t_train).long
+            self.x = x_train
+            self.t = t_train
+            self.size = x_train.shape[0]
+        else:
+            #self.x = torch.from_numpy(x_test).float
+            #self.t = torch.from_numpy(t_test).long
+            self.x = x_test
+            self.t = t_test
+            self.size = x_test.shape[0]
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self,index):
+        return self.x[index],self.t[index]
+
+def load_kuzushijiMNIST(batch_size = 500):
+    train_loader = torch.utils.data.DataLoader(
+        Dataset(True),
+        batch_size = batch_size,
+        shuffle = True,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        Dataset(False),
+        batch_size = batch_size,
+        shuffle = True,
+    )
+    
+    return {
+        'train' : train_loader,
+        'test' : test_loader
+    }
 
 
 if __name__ == '__main__':
-    torch.manual_seed(123)
-    device = torch.device('cpu')
-    model = MLP(784,50,10).to(device)
+    epoch = 5
+    history = {
+        'train_loss' : [],
+        'test_loss' : [],
+        'test_acc' : []
+    }
 
-    criterion = nn.BCELoss()
-    optimizer = optimizers.SGD(model.parameters(),lr=0.1)
+    net = Net(784,400,40,10)
+    loaders = load_kuzushijiMNIST()
+    
 
-    def compute_loss(t,y):
-        return criterion(y,t)
-
-    def train_step(x,t):
-        model.train()
-        preds = model(x)
-        #print(preds.shape)
-        loss = compute_loss(t,preds)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        return loss
-
-    def test_step(x,t):
-        x = torch.Tensor(x).to(device)
-        t = torch.Tensor(t).to(device)
-        model.eval()
-        preds = model(x)
-        loss = compute_loss(t,preds)
-        return loss,preds
-
-    epochs = 10000
-    batch_size = 500
-    n_batches = x_train.shape[0]//batch_size
-
-    for epoch in range(epochs):
-        train_loss = 0.
-        x_ = x_train.copy()
-        t_ = t_train.copy()
-        x_ = torch.Tensor(x_).to(device)
-        t_ = torch.Tensor(t_).to(device)
+    optimizer = optimizers.Adam(params = net.parameters(),lr = 0.01)
+    criterion = nn.BCELoss()    
+    for i in range(epoch):
+        loss = None
+        net.train()
         
-        for n_batch in range(n_batches):
-            start = n_batch*batch_size
-            end = start + batch_size
-            loss = train_step(x_[start:end],t_[start:end])
-            train_loss += loss.item()
-            
-        print('epoch : {}, loss : {:.3}'.format(
-            epoch + 1,
-            train_loss
-        ))
-
-
+        for j ,(data,target) in enumerate(loaders['train']):
+            data = data.float()
+            target = target.float()
+            optimizer.zero_grad()
+            output = net(data)
+            #print('flag')
+            loss = criterion(output,target)
+            loss.backward()
+            optimizer.step()
+            if j % 10 == 0:
+                print('epoch : {},batch : {}/60000,loss : {}'.format(
+                    i+1,
+                    (j+1)*128,
+                    loss.item()
+                ))
+        history['train_loss'].append(loss)
         
-
-    loss,preds = test_step(x_test,t_test)
-    test_loss = loss.item()
-    preds = np.argmax(preds.data.cpu().numpy(),axis=1)
-    ans = np.argmax(t_test,axis=1)
-    test_acc = accuracy_score(ans,preds)
-
-    print('test_loss : {:.3f}, test_acc : {:.3f}'.format(
-        test_loss,
-        test_acc
-    ))
+        net.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data,target in loaders['test']:
+                data = data.float()
+                target = target.float()
+                output = net(data)
+                test_loss += criterion(output,target).item()
+                pred = output.argmax(dim=1,keepdim = False)
+                ans = target.argmax(dim = 1,keepdim=False)
+                correct += (pred == ans).sum().item()
+        test_loss /= 10000
+        
+        print('test accuracy : {}'.format(correct/10000))
+        history['test_loss'].append(test_loss)
+        history['test_acc'].append(correct/10000)
+    
